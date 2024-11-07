457e9a26a7cce75446b3c6e635a5cb80f7f9cfa0
vietinfo123 <lenhtranchi98@gmail.com>
2024-11-07 17:55:45 +0700

    Authorization server update default configuration

--- AuthorizationServerDemo/src/main/java/DI/DefaultSecurityConfig.java
+++ AuthorizationServerDemo/src/main/java/DI/DefaultSecurityConfig.java
@@ -3,6 +3,18 @@ package DI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -12,10 +24,10 @@ public class DefaultSecurityConfig {
     SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
         http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-                .oidc(withDefaults()); // Enable OpenID Connect 1.0
+                .oidc(Customizer.withDefaults()); // Enable OpenID Connect 1.0
 
         // generate a default form login page:
-        return http.formLogin(withDefaults()).build();
+        return http.formLogin(Customizer.withDefaults()).build();
     }
 
     @Bean
@@ -23,7 +35,7 @@ public class DefaultSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest()
                         .authenticated())
-                .formLogin(withDefaults());
+                .formLogin(Customizer.withDefaults());
         return http.build();
     }
 
