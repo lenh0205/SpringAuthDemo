fb81ccbcfba822967958096e1dba4695e728a1f2
vietinfo123 <lenhtranchi98@gmail.com>
2024-11-07 17:38:50 +0700

    update resource server

---
+++ AuthorizationServerDemo/src/main/java/DI/DefaultSecurityConfig.java
@@ -0,0 +1,43 @@
+package DI;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+@EnableWebSecurity
+public class DefaultSecurityConfig {
+    @Bean
+    @Order(1)
+    SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(withDefaults()); // Enable OpenID Connect 1.0
+
+        // generate a default form login page:
+        return http.formLogin(withDefaults()).build();
+    }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest()
+                        .authenticated())
+                .formLogin(withDefaults());
+        return http.build();
+    }
+
+    // define a set of example users - use for testing
+    // create a repository with just a single admin user
+    @Bean
+    UserDetailsService users() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.builder()
+                .username("admin")
+                .password("password")
+                .passwordEncoder(encoder::encode)
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
