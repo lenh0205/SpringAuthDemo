23dcff2b2e5a4e72a6c00996bea8659ffa52584b
lenh0205 <lenhtranchi98@gmail.com>
2024-11-07 23:25:51 +0700

    update client and resource server

---
+++ ClientDemo/src/main/java/DI/SecurityConfig.java
@@ -0,0 +1,23 @@
+package DI;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login.loginPage("/oauth2/authorization/articles-client-oidc"))
+                .oauth2Client(Customizer.withDefaults());
+        return http.build();
+    }
+}
